f8f192f9cb836591f59f94d8b223537dc2cda468
ERIKA PUTRI DYATNA <161329864+ER051203@users.noreply.github.com>
2024-02-28 10:59:42 +0700

    Add files via upload
    Tugas membuat program yang membaca bilangan bulat dan membaginya dengan 3 kemudian menampilkan hasilnya dengan tiga desimal jika ada (dibulatkan)

---
+++ task2.py
@@ -0,0 +1,7 @@
+print ("---PROGRAM BILANGAN BULAT KEMUDIAN DIBAGI DENGAN ANGKA 3---") 
+
+bilangan_bulat = int(input("Masukan Bilangan Bulat : "))    #input
+rumus = float (bilangan_bulat / 3)
+print ("Setelah dibagi 3 :", rumus)
+
+
